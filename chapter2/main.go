a4f98c441bd10e0b1b54578b44323e3428179c70
Syful <sharif.syful@gmail.com>
2023-02-01 00:19:01 +0600

    Chapter 1 code edited

---
+++ chapter2/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	// fmt.Println("1 + 1 =", 1+1)
+	// fmt.Println("1 + 1 = ", 1.0+1.0)
+	// fmt.Println(true && true)
+	var x string = "Hello World"
+	fmt.Println(x)
+}
